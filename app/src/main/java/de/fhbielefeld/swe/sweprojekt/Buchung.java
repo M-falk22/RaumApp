a835cb3925fb81ed9724766620cc9821e913e01d
SeyitB <58956958+SeyitB@users.noreply.github.com>
2019-12-18 17:31:38 +0100

    Update Buchung.java

--- app/src/main/java/de/fhbielefeld/swe/sweprojekt/Buchung.java
+++ app/src/main/java/de/fhbielefeld/swe/sweprojekt/Buchung.java
@@ -24,7 +24,7 @@ public class Buchung {
     }
 
 
-    public Buchung(int startzeitStunde, int startzeitMinute) {
+    public Buchung(int startzeitStunde, int startzeitMinute) {      //Testcode
         StartzeitStunde = startzeitStunde;
         StartzeitMinute = startzeitMinute;
         EndzeitStunde = StartzeitStunde+1;
